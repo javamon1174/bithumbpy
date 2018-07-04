740425ff8677ec4d7876b4962ffc1a2245021ab0
inasie <inasie@naver.com>
2018-07-04 23:32:27 +0900

    Implemented all API

--- bithumbpy/bithumbpy.py
+++ bithumbpy/bithumbpy.py
@@ -36,21 +36,43 @@ class Bithumbpy():
             logging.error('_load_all_currency failed(%s)' % str(e))
             return None
 
-    def _get(self, url, headers=None, data=None, params=None, result_code=200):
-        resp = requests.get(url, headers=headers, data=data, params=params)
-        if resp.status_code != result_code:
+    def _get(self, url, params=None):
+        resp = requests.get(url, params=params)
+        if resp.status_code != 200:
             logging.error('get(%s) failed(%d)' % (url, resp.status_code))
             if resp.text is not None:
                 logging.error('resp: %s' % resp.text)
             return None
         return json.loads(resp.text)
 
+    def _get_sign(self, path, params, nonce):
+        data = path + chr(0) + urllib.parse.urlencode(params) + chr(0) + nonce
+        h = hmac.new(bytes(self.api_secret.encode('utf-8')), data.encode('utf-8'), hashlib.sha512)
+        api_sign = base64.b64encode(h.hexdigest().encode('utf-8'))
+        return api_sign.decode('utf-8')
+
+    def _post(self, url, params):
+        BASE_URL = 'https://api.bithumb.com'
+        path = url[len(BASE_URL):]
+        nonce = str(int(time.time() * 1000))
+        headers = {
+            'Api-Key': self.api_key,
+            'Api-Sign': self._get_sign(path, params, nonce),
+            'Api-Nonce': nonce
+        }
+        resp = requests.post(url, headers=headers, data=params)
+        if resp.status_code != 200:
+            logging.error('_post(%s) failed(%d)' % (url, resp.status_code))
+            if resp.text is not None:
+                logging.error('resp: %s' % resp.text)
+            return None
+        return json.loads(resp.text)
 
     ###############################################################
     # Public API
     ###############################################################
 
-    def ticker(self, currency='ALL'):
+    def ticker(self, currency):
         URL = 'https://api.bithumb.com/public/ticker/'
         try:
             if currency != 'ALL' and currency not in self.all_currency:
@@ -61,7 +83,7 @@ class Bithumbpy():
             logging.error('ticker() failed(%s)' % str(e))
             return None
 
-    def orderbook(self, currency='ALL', group_orders=1, count=5):
+    def orderbook(self, currency, group_orders=1, count=5):
         URL = 'https://api.bithumb.com/public/orderbook/'
         try:
             if currency != 'ALL' and currency not in self.all_currency:
@@ -82,7 +104,7 @@ class Bithumbpy():
             logging.error('orderbook() failed(%s)' % str(e))
             return None
 
-    def transaction_history(self, currency='BTC', cont_no=None, count=20):
+    def transaction_history(self, currency, cont_no=None, count=20):
         URL = 'https://api.bithumb.com/public/transaction_history/'
         try:
             if currency not in self.all_currency:
@@ -104,42 +126,231 @@ class Bithumbpy():
     ###############################################################
 
     # TODO: implement me
-    def info_account(self):
-        raise NotImplementedError
+    def info_account(self, currency):
+        URL = 'https://api.bithumb.com/info/account'
+        try:
+            if currency not in self.all_currency:
+                logging.error('invalid currency: %s' % currency)
+                return None
+            params = { 'currency': currency }
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('info_account() failed(%s)' % str(e))
+            return None
 
-    def info_balance(self):
-        raise NotImplementedError
+    def info_balance(self, currency):
+        URL = 'https://api.bithumb.com/info/balance'
+        try:
+            if currency != 'ALL' and currency not in self.all_currency:
+                logging.error('invalid currency: %s' % currency)
+                return None
+            params = { 'currency': currency}
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('info_balance() failed(%s)' % str(e))
+            return None
 
-    def info_wallet_address(self):
-        raise NotImplementedError
+    def info_wallet_address(self, currency):
+        URL = 'https://api.bithumb.com/info/wallet_address'
+        try:
+            if currency not in self.all_currency:
+                logging.error('invalid currency: %s' % currency)
+                return None
+            params = { 'currency': currency }
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('info_wallet_address() failed(%s)' % str(e))
+            return None
 
-    def info_ticker(self):
-        raise NotImplementedError
 
-    def info_orders(self):
-        raise NotImplementedError
+    def info_ticker(self, currency):
+        URL = 'https://api.bithumb.com/info/ticker'
+        try:
+            if currency not in self.all_currency:
+                logging.error('invalid currency: %s' % currency)
+                return None
+            params = {
+                'order_currency': currency,
+                'payment_currency': 'KRW'
+            }
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('info_ticker() failed(%s)' % str(e))
+            return None
 
-    def info_user_transactions(self):
-        raise NotImplementedError
+    def info_orders(self, currency, order_id, type, count=None, after=None):
+        URL = 'https://api.bithumb.com/info/orders'
+        try:
+            if currency not in self.all_currency:
+                logging.error('invalid currency: %s' % currency)
+                return None
+            if type not in ['bid', 'ask']:
+                logging.error('invalid type: %s' % type)
+                return None
+            params = {
+                'currency': currency,
+                'order_id': order_id,
+                'type': type
+            }
+            if after is not None:
+                params['after'] = after
+            if count is not None:
+                params['count'] = count
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('info_orders() failed(%s)' % str(e))
+            return None
 
-    def info_order_detail(self):
-        raise NotImplementedError
+    def info_user_transactions(self, currency, searchGb, offset=None, count=None):
+        URL = 'https://api.bithumb.com/info/user_transactions'
+        try:
+            if currency not in self.all_currency:
+                logging.error('invalid currency: %s' % currency)
+                return None
+            if searchGb not in [0, 1, 2, 3, 4, 5, 9]:
+                logging.error('invalid searchGb: %d' % searchGb)
+                return None
+            params = {
+                'currency': currency,
+                'searchGb': searchGb
+            }
+            if offset is not None:
+                params['offset'] = offset
+            if count is not None:
+                params['count'] = count
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('info_user_transactions() failed(%s)' % str(e))
+            return None
 
-    def trade_place(self):
-        raise NotImplementedError
+    def trade_place(self, currency, units, price, type):
+        URL = 'https://api.bithumb.com/trade/place'
+        try:
+            if currency not in self.all_currency:
+                logging.error('invalid currency: %s' % currency)
+                return None
+            if type not in ['bid', 'ask']:
+                logging.error('invalid type: %s' % type)
+                return None
+            params = {
+                'order_currency': currency,
+                'Payment_currency': 'KRW',
+                'type': type,
+                'price': price,
+                'units': units
+            }
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('trade_place() failed(%s)' % str(e))
+            return None
 
-    def trade_cancel(self):
-        raise NotImplementedError
+    def info_order_detail(self, currency, order_id, type):
+        URL = 'https://api.bithumb.com/info/order_detail'
+        try:
+            if currency not in self.all_currency:
+                logging.error('invalid currency: %s' % currency)
+                return None
+            if type not in ['bid', 'ask']:
+                logging.error('invalid type: %s' % type)
+                return None
+            params = {
+                'currency': currency,
+                'type': type,
+                'order_id': order_id
+            }
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('info_order_detail() failed(%s)' % str(e))
+            return None
 
-    def trade_btc_withdrawal(self):
-        raise NotImplementedError
+    def trade_cancel(self, currency, order_id, type):
+        URL = 'https://api.bithumb.com/trade/cancel'
+        try:
+            if currency not in self.all_currency:
+                logging.error('invalid currency: %s' % currency)
+                return None
+            if type not in ['bid', 'ask']:
+                logging.error('invalid type: %s' % type)
+                return None
+            params = {
+                'currency': currency,
+                'type': type,
+                'order_id': order_id
+            }
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('trade_cancel() failed(%s)' % str(e))
+            return None
 
-    def trade_krw_dposit(self):
-        raise NotImplementedError
+    # not tested
+    def trade_btc_withdrawal(self, currency, units, address, destination):
+        URL = 'https://api.bithumb.com/trade/btc_withdrawal'
+        try:
+            if currency not in self.all_currency:
+                logging.error('invalid currency: %s' % currency)
+                return None
+            params = {
+                'currency': currency,
+                'address': address,
+                'destination': destination,
+                'units': units
+            }
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('trade_btc_withdrawal() failed(%s)' % str(e))
+            return None
 
-    def trade_market_buy(self):
-        raise NotImplementedError
 
-    def trade_market_sell(self):
-        raise NotImplementedError
+    def trade_krw_deposit(self):
+        URL = 'https://api.bithumb.com/trade/krw_deposit'
+        try:
+            params = {}
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('trade_krw_deposit() failed(%s)' % str(e))
+            return None
+
+    # not tested
+    def trade_krw_withdrawal(self, bank, account, price):
+        URL = 'https://api.bithumb.com/trade/krw_withdrawal'
+        try:
+            params = {
+                'bank': bank,
+                'account': account,
+                'price': price
+            }
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('trade_krw_withdrawal() failed(%s)' % str(e))
+            return None
+
+    def trade_market_buy(self, currency, units):
+        URL = 'https://api.bithumb.com/trade/market_buy'
+        try:
+            if currency not in self.all_currency:
+                logging.error('invalid currency: %s' % currency)
+                return None
+            params = {
+                'currency': currency,
+                'units': units
+            }
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('trade_market_buy() failed(%s)' % str(e))
+            return None
+
+    def trade_market_sell(self, currency, units):
+        URL = 'https://api.bithumb.com/trade/market_sell'
+        try:
+            if currency not in self.all_currency:
+                logging.error('invalid currency: %s' % currency)
+                return None
+            params = {
+                'currency': currency,
+                'units': units
+            }
+            return self._post(URL, params)
+        except Exception as e:
+            logging.error('trade_market_sell() failed(%s)' % str(e))
+            return None
 
--- test/test.py
+++ test/test.py
@@ -1,8 +1,9 @@
 import logging
+import apikey
 from bithumbpy import Bithumbpy
 
-KEY = ''
-SECRET = ''
+KEY = apikey.KEY
+SECRET = apikey.SECRET
 
 logging.basicConfig(level=logging.DEBUG)
 bithumb = Bithumbpy(api_key=KEY, api_secret=SECRET)
@@ -12,41 +13,68 @@ bithumb = Bithumbpy(api_key=KEY, api_secret=SECRET)
 ###############################################################
 
 # ticker
-# logging.debug('================================')
-# logging.debug(bithumb.ticker('QTUM'))
-# logging.debug('================================')
+# logging.debug(bithumb.ticker('ALL'))
 # logging.debug(bithumb.ticker('ADA'))
-# logging.debug('================================')
-# logging.debug(bithumb.ticker('AAA'))
-# logging.debug('================================')
 
 # orderbook
-# logging.debug('================================')
 # logging.debug(bithumb.orderbook('BTC'))
-# logging.debug('================================')
 # logging.debug(bithumb.orderbook('ETH', group_orders=0))
-# logging.debug('================================')
 # logging.debug(bithumb.orderbook('ETH', count=10))
-# logging.debug('================================')
 # logging.debug(bithumb.orderbook('ETH', group_orders=0, count=50))
-# logging.debug('================================')
 # logging.debug(bithumb.orderbook('AAA'))
-# logging.debug('================================')
 
 # transaction_history
-logging.debug('================================')
-logging.debug(bithumb.transaction_history('BTC'))
-logging.debug('================================')
-logging.debug(bithumb.transaction_history('ADA', count=1))
-logging.debug('================================')
-logging.debug(bithumb.transaction_history('ADA', cont_no=10000))
-logging.debug('================================')
-logging.debug(bithumb.transaction_history('ADA', count=10, cont_no=10000))
-logging.debug('================================')
-logging.debug(bithumb.transaction_history('AAA'))
-logging.debug('================================')
+# logging.debug(bithumb.transaction_history('BTC'))
+# logging.debug(bithumb.transaction_history('ADA', count=1))
+# logging.debug(bithumb.transaction_history('ADA', cont_no=10000))
+# logging.debug(bithumb.transaction_history('ADA', count=10, cont_no=10000))
+# logging.debug(bithumb.transaction_history('AAA'))
 
 ###############################################################
 # Private API
 ###############################################################
-# TODO
+# info/account
+# logging.debug(bithumb.info_account('QTUM'))
+
+# info/balance
+# logging.debug(bithumb.info_balance())
+# logging.debug(bithumb.info_balance('AE'))
+
+# info/wallet_address
+# logging.debug(bithumb.info_wallet_address())
+# logging.debug(bithumb.info_wallet_address('QTUM'))
+
+# info/ticker
+# logging.debug(bithumb.info_ticker('ALL'))
+# logging.debug(bithumb.info_ticker('AE'))
+
+# info/orders
+# logging.debug(bithumb.info_orders('VEN', '1530713502924607', 'ask'))
+# logging.debug(bithumb.info_ticker('AE'))
+
+# info/user_transactions
+# logging.debug(bithumb.info_user_transactions('AE', 0))
+
+# trade/place
+# logging.debug(bithumb.trade_place('VEN', 1.0, 3500, 'ask'))
+
+# info/order_detail
+# logging.debug(bithumb.info_order_detail('VEN', '1530713673955645', 'ask'))
+
+# trade/cancel
+# logging.debug(bithumb.trade_cancel('VEN', '1530713673955645', 'ask'))
+
+# trade/btc_withdrawal
+# logging.debug(bithumb.trade_btc_withdrawal('VEN', '1530713673955645', 'ask'))
+
+# trade/krw_deposit
+# logging.debug(bithumb.trade_krw_deposit())
+
+# trade/krw_withdrawal
+# logging.debug(bithumb.trade_krw_withdrawal('002', 'your account', 100000000))
+
+# trade/market_buy
+# logging.debug(bithumb.trade_market_buy('QTUM', 1))
+
+# trade/market_sell
+# logging.debug(bithumb.trade_market_sell('VEN', 1))
